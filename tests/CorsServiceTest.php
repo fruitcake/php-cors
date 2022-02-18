8b9a8c1b01ece9bd81f33d7e697dc0951e4dca58
Barry vd. Heuvel <barry@fruitcake.nl>
2022-02-18 20:42:56 +0100

    Test option normalisation

--- tests/CorsServiceTest.php
+++ tests/CorsServiceTest.php
@@ -13,6 +13,7 @@
 namespace Fruitcake\Cors\Tests;
 
 use Fruitcake\Cors\CorsService;
+use Fruitcake\Cors\Exceptions\InvalidOptionException;
 use PHPUnit\Framework\TestCase;
 
 class CorsServiceTest extends TestCase
@@ -36,7 +37,6 @@ class CorsServiceTest extends TestCase
     {
         $service = new CorsService();
         $this->assertInstanceOf(CorsService::class, $service);
-
     }
 
     /**
@@ -46,6 +46,73 @@ class CorsServiceTest extends TestCase
     {
         $service = new CorsService([]);
         $this->assertInstanceOf(CorsService::class, $service);
+    }
 
+    /**
+     * @test
+     */
+    public function it_throws_exception_on_invalid_exposed_headers()
+    {
+        $this->expectException(InvalidOptionException::class);
+
+        $service = new CorsService(['exposedHeaders' => true]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_exception_on_invalid_origins_array()
+    {
+        $this->expectException(InvalidOptionException::class);
+
+        $service = new CorsService(['allowedOrigins' => 'string']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_normalizes_wildcard_options()
+    {
+        $origins = ['*'];
+
+        $service = new CorsService(['allowedOrigins' => $origins]);
+        $this->assertInstanceOf(CorsService::class, $service);
+
+        $this->assertEquals(true, $this->getOptionsFromService($service)['allowedOrigins']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_origin_patterns()
+    {
+        $service = new CorsService(['allowedOrigins' => ['*.mydomain.com']]);
+        $this->assertInstanceOf(CorsService::class, $service);
+
+        $patterns = $this->getOptionsFromService($service)['allowedOriginsPatterns'];
+        $this->assertEquals(['#^.*\.mydomain\.com\z#u'], $patterns);
+    }
+
+    /**
+     * @test
+     */
+    public function it_normalizes_underscore_options()
+    {
+        $origins = ['localhost'];
+
+        $service = new CorsService(['allowed_origins' => $origins]);
+        $this->assertInstanceOf(CorsService::class, $service);
+
+        $this->assertEquals($origins, $this->getOptionsFromService($service)['allowedOrigins']);
+    }
+
+    private function getOptionsFromService(CorsService $service)
+    {
+        $reflected = new \ReflectionClass($service);
+
+        $property = $reflected->getProperty('options');
+        $property->setAccessible(true);
+
+        return $property->getValue($service);
     }
 }
